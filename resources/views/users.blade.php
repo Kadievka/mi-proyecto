75042149900928ebd78aeb4aac037c7ee442a03e
Kadievka <kadievka.s.ks@gmail.com>
2019-08-24 18:56:21 -0430

    Ahora estamos utilizando Blade en la vista usuarios.blade.php

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -65,11 +65,11 @@
         -->
     </head>
     <body>
-        <h1><?php echo e($title) ?></h1>
+        <h1>{{$title}}</h1>
         <ul>
-            <?php foreach ($users as $user){ ?>
-            <li><?php echo e($user) ?></li>
-            <?php };?>
+            @foreach ($users as $user)
+                <li>{{($user)}}</li>
+            @endforeach
         </ul>
     </body>
 </html>
